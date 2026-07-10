977a3c60bb27ba6a17f238777ef8a1e4511260bb
Benjamin Leray <b.leray35@gmail.com>
2026-07-10 12:30:50 +0200

    extract tmp component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,25 +1,19 @@
 "use client";
-import { getUserFolders } from "@/api/getUserFolders";
-import { useState } from "react";
-export default function Home() {
-  const [foldersJson, setFoldersJson] = useState({ folders: [] });
 
-  const handleOnGetUserFolders = async () => {
-    const data = await getUserFolders();
-    setFoldersJson(data);
+import { FoldersList } from "@/components/FoldersList";
+export default function Home() {
+  const testFolder = {
+    id: 9434181,
+    name: "test_private",
+    count: 1,
+    resource_url:
+      "https://api.discogs.com/users/benyarel/collection/folders/9434181",
   };
 
   return (
     <main>
-      Collectog
-      <button onClick={handleOnGetUserFolders}>Get folders</button>
-      {foldersJson?.folders && (
-        <ul>
-          {foldersJson.folders.map((folder: any) => (
-            <li key={folder.id}>{folder.name}</li>
-          ))}
-        </ul>
-      )}
+      <h1>Collectog</h1>
+      <FoldersList />
     </main>
   );
 }
---
+++ src/components/FoldersList.tsx
@@ -0,0 +1,20 @@
+import { getUserFolders } from "@/api/getUserFolders";
+import { useState } from "react";
+export const FoldersList = () => {
+  const [foldersJson, setFoldersJson] = useState({ folders: [] });
+
+  const handleOnGetUserFolders = async () => {
+    const data = await getUserFolders();
+    setFoldersJson(data);
+  };
+  return (
+    <>
+      <button onClick={handleOnGetUserFolders}>Get folders</button>
+      <ul>
+        {foldersJson.folders.map((folder: any) => (
+          <li key={folder.id}>{folder.name}</li>
+        ))}
+      </ul>
+    </>
+  );
+};
