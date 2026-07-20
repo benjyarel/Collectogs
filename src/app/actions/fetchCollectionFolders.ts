1f8371949a60471a13ef5cff1d25cdb4d1a35ef8
Benjamin Leray <b.leray35@gmail.com>
2026-07-20 14:20:04 +0200

    remove console log

--- src/app/actions/fetchCollectionFolders.ts
+++ src/app/actions/fetchCollectionFolders.ts
@@ -6,13 +6,11 @@ export async function fetchCollectionFolders(
   username: string,
 ): Promise<{ success: boolean; data: CollectionFolder[] }> {
   if (!username) {
-    console.log("pas de username")
     return { success: false, data: [] };
   }
   const folders = await getCollectionFolders({ username });
 
   if (!folders) {
-    console.log("pas de folders")
     return { success: false, data: [] };
   }
 
