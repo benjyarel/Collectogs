10d21fd060b13cd8dd87a2d35a3f9f12c257c06d
Benjamin Leray <b.leray35@gmail.com>
2026-07-20 11:20:07 +0200

    cleanup

--- src/app/components/FolderSelect/index.tsx
+++ src/app/components/FolderSelect/index.tsx
@@ -1,16 +1,17 @@
 "use client"
 
+import { ChangeEvent } from 'react'
 import { CollectionFolder } from "@/app/types";
-import { ChangeEvent } from "react"
-export const FolderSelect = ({ folders }: { folders: CollectionFolder[] }) => {
-    const handleOnChangeFolder = (e: ChangeEvent<HTMLSelectElement>) => {
-        console.log(e.target.value)
 
+export const FolderSelect = ({ folders }: { folders: CollectionFolder[] }) => {
+    const onFoldersChange = (e: ChangeEvent<HTMLSelectElement>) => {
+        console.log(e.target.value)
+        return e.target.value;
     }
     return (
         <>
             <label htmlFor="folders">Folder:</label>
-            <select onChange={handleOnChangeFolder} name="collection folders" id="folders">
+            <select onChange={onFoldersChange} name="collection folders" id="folders">
                 {folders.map(({ id, name, count }) => {
                     return (<option key={id} value={id}>
                         {name} ({count})
--- src/app/components/LeftPanel/index.tsx
+++ src/app/components/LeftPanel/index.tsx
@@ -1,10 +1,12 @@
 
+import { ChangeEvent } from "react"
 import { fetchCollectionFolders } from "@/app/actions/fetchCollectionFolders";
 import { DiscogUser } from "@/app/types";
 import styles from "./index.module.css"
 import { FolderSelect } from "@/app/components/FolderSelect";
 export const LeftPanel = async ({ username }: { username: DiscogUser["username"] }) => {
     const { data: folders } = await fetchCollectionFolders(username);
+
 
     return (
         <div className={"section-block " + styles["left-panel"]}>
