c103f04a5dab5b5a7ddd99b81ce783639ff0d4a6
Benjamin Leray <b.leray35@gmail.com>
2026-07-21 18:05:13 +0200

    [chore] move serialization logic in actions (#23)
    * extract mocked data
    
    * clean types
    
    * fix ts
    
    * better promise handling type
    
    * update ts in specs
    
    * refactor: filter albums in fetchArtistReleases and clean up getArtistReleases
    
    * refactor: standardize response structure in fetchCollectionFolders and update usage in Home component

--- src/app/actions/fetchArtistReleases.ts
+++ src/app/actions/fetchArtistReleases.ts
@@ -1,7 +1,14 @@
 "use server"
 import { Artist } from "@/app/types";
 import { getArtistReleases } from "@/app/lib/discog/getArtistReleases"
+
 export const fetchArtistReleases = async (artist: Artist) => {
-    const data = await getArtistReleases(artist.name);
-    return data
+    const releases = await getArtistReleases(artist.name);
+
+
+    const mainReleases = releases.filter(release =>
+        release.format?.includes("Album") && !release.format?.includes("Unofficial Release")
+    );
+
+    return { success: true, releases: mainReleases }
 }
--- src/app/actions/fetchCollectionFolders.ts
+++ src/app/actions/fetchCollectionFolders.ts
@@ -4,15 +4,15 @@ import { getCollectionFolders } from "@/app/lib/discog/getCollectionFolders";
 
 export async function fetchCollectionFolders(
   username: string,
-): Promise<{ success: boolean; data: CollectionFolder[] }> {
+): Promise<{ success: boolean; folders: CollectionFolder[] }> {
   if (!username) {
-    return { success: false, data: [] };
+    return { success: false, folders: [] };
   }
   const folders = await getCollectionFolders({ username });
 
   if (!folders) {
-    return { success: false, data: [] };
+    return { success: false, folders: [] };
   }
 
-  return { success: true, data: folders };
+  return { success: true, folders };
 }
