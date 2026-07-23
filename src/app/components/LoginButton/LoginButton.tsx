ff89633ff8ab4765a149f405619f08b693b66d55
Benjamin Leray <b.leray35@gmail.com>
2026-07-23 15:46:23 +0200

    change file

--- src/app/components/LoginButton/LoginButton.tsx
+++ src/app/components/LoginButton/LoginButton.tsx
@@ -1,16 +1,5 @@
+import { getRequestToken } from "./getRequestToken";
+
 export const LoginButton = () => {
-    const getRequestToken = async () => {
-        try {
-            const res = await fetch("/api/discog/auth");
-
-            const textData = await res.text();
-            const params = new URLSearchParams(textData);
-            const { oauth_token } = Object.fromEntries(params);
-
-            window.location.href = `https://www.discogs.com/oauth/authorize?oauth_token=${oauth_token}`;
-        } catch (err) {
-            console.error(err);
-        }
-    };
     return <button className="btn" onClick={getRequestToken}>Login</button>
 }
