239662cf706fee18fc1536a905ef708164f0ce01
Benjamin Leray <b.leray35@gmail.com>
2026-07-27 17:22:53 +0200

    release card

---
+++ src/app/components/ArtistHeader/ArtistHeader.test.tsx
@@ -0,0 +1,15 @@
+import { expect, test, describe } from 'vitest'
+import { render, screen } from '@testing-library/react'
+import { ArtistHeader } from '.'
+
+describe("renders", () => {
+    test("the word 'Artist'", () => {
+        render(<ArtistHeader artistName="Daft Punk" />)
+        expect(screen.getByText("Artist")).toBeDefined()
+    })
+
+    test("the artist name", () => {
+        render(<ArtistHeader artistName="Daft Punk" />)
+        expect(screen.getByText("Daft Punk")).toBeDefined()
+    })
+})
--- src/app/components/Content/Content.tsx
+++ src/app/components/Content/Content.tsx
@@ -1,21 +1,24 @@
 import { Release } from "@/app/types";
 import styles from "./Content.module.css"
-
+import { ArtistHeader } from "@/app/components/ArtistHeader"
+import { ReleaseCard } from "@/app/components/ReleaseCard"
 export const Content = ({ releases }: { releases: Release[] }) => {
+
     if (!releases.length) {
         return <div className={styles.content}>Select a folder to see its albums.</div>;
     }
 
+    const artistName = releases[0].artistName
     const mainReleases = releases.filter((release) => release.category === "master");
     const uncategorizedReleases = releases.filter((release) => release.category === "uncategorized");
 
     return (
         <div className={styles.content}>
+            <ArtistHeader artistName={artistName} />
             <ul>
                 {mainReleases.map((release) => (
                     <li key={release.id}>
-                        {release.title}
-                        {!!release.year && (<>({release.year})</>)}
+                        <ReleaseCard release={release} />
                     </li>
                 ))}
             </ul>
@@ -25,8 +28,7 @@ export const Content = ({ releases }: { releases: Release[] }) => {
                     <ul>
                         {uncategorizedReleases.map((release) => (
                             <li key={release.id}>
-                                {release.title}
-                                {!!release.year && (<>({release.year})</>)}
+                                <ReleaseCard release={release} />
                             </li>
                         ))}
                     </ul>
@@ -35,3 +37,4 @@ export const Content = ({ releases }: { releases: Release[] }) => {
         </div>
     )
 }
+
